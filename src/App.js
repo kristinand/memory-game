200b863294c918ae0e2fa1f84a1a3644c3b13933
Kristina Andreeva <_>
2021-02-02 19:35:09 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
