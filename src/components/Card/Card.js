05863aa0aacc180e54290df468edf61e2f46b1fa
Kristina Andreeva <_>
2021-02-04 20:35:29 +0300

    fix Card hover style effects

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,18 +4,24 @@ import classes from './Card.css';
 const card = (props) => {
   const clicked = (event) => {
     const card = event.target.closest('.Card');
-		card.style.transform = 'rotateY(180deg)';
-		setTimeout(() => {
-			card.style.transform = 'rotateY(0deg)';
-		}, 7000);
+    card.style.transform = 'rotateY(180deg)';
+    card.style.boxShadow = '-2px 2px 0.1em 3px hsla(0, 0%, 70%, 0.3)';
+    setTimeout(() => {
+      card.style.transform = 'rotateY(0deg)';
+      card.style.boxShadow = '2px 2px 0.1em 3px hsla(0, 0%, 70%, 0.3)';
+    }, 7000);
   };
 
   return (
-    <div onClick={clicked} className={['Card', classes.Card].join(' ')}>
-      <div className={classes.CardCover} style={{ backgroundColor: props.coverColor }}><span>?</span></div>
-      <div className={classes.CardColor} style={{ backgroundColor: props.color}}>
-				<span className={'pattern'}>{props.pattern}</span>
-			</div>
+    <div className={classes.wrapper}>
+      <div onClick={clicked} className={['Card', classes.Card].join(' ')}>
+        <div className={classes.CardCover} style={{ backgroundColor: props.coverColor }}>
+          <span>?</span>
+        </div>
+        <div className={classes.CardColor} style={{ backgroundColor: props.color }}>
+          <span className={'pattern'}>{props.pattern}</span>
+        </div>
+      </div>
     </div>
   );
 };
