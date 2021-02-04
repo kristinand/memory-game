42c0d036c49e8ecddb8f481d4a6688aebcaf4041
Kristina Andreeva <_>
2021-02-04 20:41:45 +0300

    refactor: move cover color to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Game from './containers/Game/Game';
 import Header from './containers/Header/Header';
+import { generateRandomColor } from './utils/functions';
 
 class App extends Component {
   state = {
@@ -10,18 +11,23 @@ class App extends Component {
   onChangeLevelHandler(param) {
     let level = this.state.level;
     if (param === 'inc' && level < 6) {
-      level +=1;
+      level += 1;
     } else if (param === 'dec' && level > 1) {
-      level -=1;
+      level -= 1;
     }
-    this.setState({level: level});
+    this.setState({ level: level });
   }
 
   render() {
+    const color = generateRandomColor(40, 40, 60, 60);
     return (
       <Fragment>
-        <Header changeLevel={(param) => this.onChangeLevelHandler(param)} level={this.state.level} />
-        <Game level={this.state.level} />
+        <Header
+          buttonColor={color}
+          changeLevel={(param) => this.onChangeLevelHandler(param)}
+          level={this.state.level}
+        />
+        <Game coverColor={color} level={this.state.level} />
       </Fragment>
     );
   }
--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getRandomNumber, shuffleList, generateRandomColor, listToArray } from '../../utils/functions';
-import CardRow from '../../components/CardRow/CardRow';
+import CardRow from '../../components/CardRow';
 
 const game = (props) => {
   const [cards, setCards] = useState([]);
@@ -16,7 +16,6 @@ const game = (props) => {
 
   const fillCards = (cards) => {
     let patterns = ['☯', '◑', '◐', '◒', '◓', '♡', '♥', '☁', '☀', '♨', '♦', '❀'];
-    const coverColor = generateRandomColor(40, 40, 60, 60);
 
     for (let i = 0; i < props.level * 2; i++) {
       const color = generateRandomColor();
@@ -24,15 +23,27 @@ const game = (props) => {
       const patternNumber = getRandomNumber(0, patterns.length);
       const pattern = patterns[patternNumber];
       patterns = [...patterns.slice(0, patternNumber), ...patterns.slice(patternNumber + 1)];
-      cards.push({ key: '1' + props.level + i + keyPart, color: color, pattern: pattern, coverColor: coverColor });
-      cards.push({ key: '2' + props.level + i + keyPart, color: color, pattern: pattern, coverColor: coverColor });
+      cards.push({
+        key: '1' + props.level + i + keyPart,
+        color: color,
+        pattern: pattern,
+        coverColor: props.coverColor,
+      });
+      cards.push({
+        key: '2' + props.level + i + keyPart,
+        color: color,
+        pattern: pattern,
+        coverColor: props.coverColor,
+      });
     }
     return cards;
   };
 
   return (
     <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column', height: '70vh', justifyContent: 'center' }}>
-      {cards.map((cardsRow, i) => <CardRow key={i} cards={cardsRow}/>)}
+      {cards.map((cardsRow, i) => (
+        <CardRow key={i} cards={cardsRow} />
+      ))}
     </div>
   );
 };
