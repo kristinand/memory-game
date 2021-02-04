2cd6497892e90ed2f7ce80a41166e7f81de90548
Kristina Andreeva <_>
2021-02-04 20:44:02 +0300

    add IconButtons to Header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import classes from './Header.css'
+import React, { useEffect, useState } from 'react';
+import { formatTime } from '../../utils/functions';
+import classes from './Header.css';
+import IconButton from '../../components/IconButton';
+
+import Refresh from '@assets/icons/refresh.svg';
+import Pause from '@assets/icons/pause.svg';
+import Play from '@assets/icons/play.svg';
+import Screen from '@assets/icons/screen.svg';
+import Sound2 from '@assets/icons/sound2.svg';
+import Music2 from '@assets/icons/music2.svg';
 
 const header = (props) => {
-	return (
-		<div className={classes.Header}>
-			<span>level: {props.level}</span>
-			<button onClick={() => props.changeLevel('inc')} style={{margin: '0 0.5em'}}>+</button>
-			<button onClick={() => props.changeLevel('dec')} style={{margin: '0 0.5em'}}>-</button>
-		</div>
-	)
-}
+  return (
+    <div className={classes.Header}>
+      <span>
+        <span onClick={() => props.changeLevel('dec')}>{'<'}</span>
+        <span className={classes.level}>level: {props.level}</span>
+        <span onClick={() => props.changeLevel('inc')}>{'>'}</span>
+      </span>
+
+      <span className={classes.right}>
+        <IconButton color={props.buttonColor} component={Play} />
+        <IconButton color={props.buttonColor} component={Pause} />
+        <IconButton color={props.buttonColor} component={Refresh} />
+        <IconButton color={props.buttonColor} component={Screen} />
+        <IconButton color={props.buttonColor} component={Sound2} />
+        <IconButton color={props.buttonColor} component={Music2} />
+      </span>
+    </div>
+  );
+};
 
 export default header;
