ccadea7bd919a478ee78ab775d5bb208b2e7cf62
Kristina Andreeva <_>
2021-02-14 23:09:19 +0300

    add input name check

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -8,20 +8,33 @@ const menu = (props) => {
   const dispatch = useDispatch();
   const store = useSelector((store) => store);
   const [player, setPlayer] = useState(store.player);
+  const [isHelperTextVisible, setIsHelperTextVisible] = useState(false);
+  let inputClasses = isHelperTextVisible ? [classes.input, classes.inputDanger].join(' ') : classes.input;
+
+  if (player.length > 0 && isHelperTextVisible) setIsHelperTextVisible(false);
 
   return (
-    <div className={classes.Menu}>
-      <input
-        className={classes.Input}
-        type="text"
-        id="name"
-        value={player}
-        onChange={(event) => setPlayer(event.target.value)}
-        placeholder="Your name"
-				autocomplete="off"
-      />
+    <div className={classes.menu}>
+      <div className={classes.inputContainer}>
+        <input
+          className={inputClasses}
+          type="text"
+          id="name"
+          value={player}
+          onChange={(event) => setPlayer(event.target.value)}
+          placeholder="Your name"
+          autoComplete="off"
+        />
+        {isHelperTextVisible ? <p className={classes.helperText}>Please, enter your name</p> : ''}
+      </div>
       <span className="separator">♥ ☀ ♦</span>
-      <MenuButton onClick={() => dispatch(actions.startGame(player))} title="New Game" />
+      <MenuButton onClick={() => {
+        if (player.length === 0) { 
+          setIsHelperTextVisible(true);
+        } else {
+          dispatch(actions.startGame(player))
+        }
+        }} title="New Game" />
       <MenuButton onClick={() => {}} disabled={true} title="Continue" />
       <MenuButton onClick={() => {}} title="Rating" />
       <MenuButton onClick={() => {}} title="Settings" />
