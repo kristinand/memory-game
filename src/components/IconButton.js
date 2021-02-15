cbf7a5f0bbd1dae003ab5c4693700d859a5b9f4a
Kristina Andreeva <_>
2021-02-15 21:30:33 +0300

    refactor IconButton styles

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -9,16 +9,35 @@ const Button = styled.span`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  transition: 0.1s ease-in-out;
+  vertical-align: middle;
+  transform-style: preserve-3d;
+  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), background 150ms cubic-bezier(0, 0, 0.58, 1);
+  }
   &:hover ${Back} {
     position: relative;
     background-color: #fff;
-    box-shadow: 2px 2px 0.1em 1px hsla(0, 0%, 30%, 0.3);
     border-radius: 0.2em;
+    &::before {
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: #eee;
+      border-radius: inherit;
+      box-shadow: 0 0.1em 0.1em 0 hsla(0, 0%, 30%, 0.3);
+      transform: translate3d(0, 0.25rem, -1em);
+      transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
   }
   &:active ${Back} {
-    top: 2px;
-    box-shadow: 0px 0px 1px 0px hsla(0, 0%, 30%, 0.3);
+    transform: translate(0em, 0.25rem);
+    &::before {
+      box-shadow: 0 0 hsla(0, 0%, 30%, 0.3);
+      transform: translate3d(0, 0, -1em);
+    }
   }
 `;
 
