3a62a21111f49f41653f2ad0cfd2b5c332521c3f
Kristina Andreeva <_>
2021-02-16 22:34:31 +0300

    refactor Rating

--- src/containers/Rating/Rating.js
+++ src/containers/Rating/Rating.js
@@ -1,51 +1,49 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './Rating.css';
 import { formatTime } from '../../utils/functions';
-import Back from '@assets/icons/left.svg';
-import IconButton from '../../components/IconButton';
+import Header from '../Header/Header';
 
-const rating = (props) => {
-	const today = new Date();
-	let data = [
-		{name: 'Bob', score: 123, date: today},
-		{name: 'Tom', score: 243, date: today},
-		{name: 'Jess', score: 231, date: today, current: true},
-		{name: 'Lily', score: 100, date: today},
-	];
+const Rating = () => {
+  const today = new Date();
+  let data = [
+    { name: 'Bob', score: 123, date: today },
+    { name: 'Tom', score: 243, date: today },
+    { name: 'Jess', score: 231, date: today, current: true },
+    { name: 'Lily', score: 100, date: today },
+  ];
 
-	data = data.sort((prev, cur) => prev.score - cur.score);
+  data = data.sort((prev, cur) => prev.score - cur.score);
 
-	 const playersData = data.map((player, i) => {
-		 const playerClass = player.current ? [classes.player, classes.current].join(' ') : classes.player;
-		 const formattedDate = `
-		 ${player.date.getDate().toString().padStart(2, 0)}-${(player.date.getMonth() + 1).toString().padStart(2, 0)}-${player.date.getFullYear()}
-		 ${player.date.getHours().toString().padStart(2, 0)}:${player.date.getMinutes().toString().padStart(2, 0)}
-		 `;
-		 return (
-			 <div key={player.name} className={playerClass}>
-				 <span>{i + 1}</span>
-				 <span>{player.name}</span>
-				 <span>{formatTime(player.score)}</span>
-				 <span>{formattedDate}</span>
-			 </div>
-		 )
-	 })
-
+  const playersData = data.map((player, i) => {
+    const playerClass = player.current ? [classes.player, classes.current].join(' ') : classes.player;
+    const formattedDate = `${player.date.getDate().toString().padStart(2, 0)}-${(player.date.getMonth() + 1)
+      .toString()
+      .padStart(2, 0)}-${player.date.getFullYear()}
+    ${player.date.getHours().toString().padStart(2, 0)}:${player.date.getMinutes().toString().padStart(2, 0)}`;
+    return (
+      <div key={player.name} className={playerClass}>
+        <span>{i + 1}</span>
+        <span>{player.name}</span>
+        <span>{formatTime(player.score)}</span>
+        <span>{formattedDate}</span>
+      </div>
+    );
+  });
 
   return (
-    <div className={classes.Rating}>
-			<IconButton component={Back} text="Back to Menu"/>
-			<div className={classes.scores}>
-				<div className={classes.header}>
-					<span>Position</span>
-					<span>Name</span>
-					<span>Completed Time</span>
-					<span>Date</span>
-				</div>
-				{playersData}
-			</div>
-    </div>
+    <Fragment>
+      <Header title="Rating" />
+      <div className={classes.Rating}>
+        <div className={classes.header}>
+          <span>Position</span>
+          <span>Name</span>
+          <span>Completed Time</span>
+          <span>Date</span>
+        </div>
+        {playersData}
+      </div>
+    </Fragment>
   );
 };
 
-export default rating;
+export default Rating;
