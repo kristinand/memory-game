1ad25b4d180edd167b1f08f0ec063247cc62bdf8
Kristina Andreeva <_>
2021-02-16 23:21:20 +0300

    add perfomance webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
     open: false,
     historyApiFallback: true,
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   module: {
     rules: [
       {
