ad1782e44deef279aa7e3c34a8cb82df7824fc82
Kristina Andreeva <_>
2021-02-16 23:20:14 +0300

    fix webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: ['@babel/polyfill', './index.js'],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '/public/'),
+    path: path.resolve(__dirname, 'public/'),
   },
   resolve: {
     extensions: ['.js', '.json'],
