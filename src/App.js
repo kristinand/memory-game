dc405ae4ef77fd961108091060263027988516dc
Kristina Andreeva <_>
2021-02-16 22:32:26 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Fragment } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Game from './containers/Game/Game';
-import Header from './containers/Header/Header';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Game from './containers/Game';
 import Menu from './containers/Menu/Menu';
+import Rating from './containers/Rating/Rating';
+import Settings from './containers/Settings/Settings';
 
 const app = () => {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
+  const player = useSelector(state => state.player);
 
-  let screen = <Menu />;
+  let routes = (
+    <Switch>
+      <Route path="/rating" component={Rating} />
+      <Route path="/settings" component={Settings} />
+      <Route path="/" component={Menu} />
+      <Redirect to="/" />
+    </Switch>
+  );
 
-  if (state.isGameStarted) {
-    screen = (
-      <div>
-        <Header />
-        <Game />
-      </div>
-    );
+  if (player.length > 0) {
+    routes = (
+      <Switch>
+        <Route path="/game" component={Game} />
+        <Route path="/rating" component={Rating} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/" component={Menu} />
+        <Redirect to="/" />
+      </Switch>
+    )
   }
 
-  return <Fragment>{screen}</Fragment>;
+  return (
+    <HashRouter>
+      {routes}
+    </HashRouter>
+  );
 };
 
 export default app;
