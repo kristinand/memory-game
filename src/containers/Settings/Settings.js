c820fbe06601fb11022ef0072d727546f45fe52c
Kristina Andreeva <_>
2021-02-16 22:34:46 +0300

    add Settings page

---
+++ src/containers/Settings/Settings.js
@@ -0,0 +1,53 @@
+import React, { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as actions from '../../store/actions';
+import classes from './Settings.css';
+import Header from '../Header/Header';
+
+const Settings = () => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const onVolumeChangeHandler = (type, value) => {
+    if (value < 0 || value > 1 || isNaN(+value)) return;
+    dispatch(actions.changeVolume(type, +value));
+  };
+
+  return (
+    <Fragment>
+      <Header title="Settings" />
+      <div className={classes.Settings}>
+        <div className={classes.settingsElement}>
+          <span>Music Volume</span>
+          <div className={classes.inputContainer}>
+            <input
+              className={classes.input}
+              type="number"
+              onChange={(event) => onVolumeChangeHandler('music', event.target.value)}
+              value={state.musicVolume}
+              max={1}
+              min={0}
+              step={0.1}
+            />
+          </div>
+        </div>
+        <div className={classes.settingsElement}>
+          <span>Sounds Volume</span>
+          <div className={classes.inputContainer}>
+            <input
+              className={classes.input}
+              type="number"
+              onChange={(event) => onVolumeChangeHandler('sound', event.target.value)}
+              value={state.soundVolume}
+              max={1}
+              min={0}
+              step={0.1}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Settings;
