68e966353b1a083a79ebd779962dcfc9d0dd34d1
Kristina Andreeva <_>
2021-02-17 21:06:38 +0300

    add player name to localStorage

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -8,28 +8,42 @@ import NameInput from '../../components/NameInput/NameInput';
 const Menu = (props) => {
   const dispatch = useDispatch();
   const store = useSelector((store) => store);
-  const [player, setPlayer] = useState(store.player);
+  const [player, setPlayer] = useState(localStorage.getItem('player') || store.player);
   const [isHelperTextVisible, setIsHelperTextVisible] = useState(false);
   if (player.length > 0 && isHelperTextVisible) setIsHelperTextVisible(false);
 
   const onNewGameHandler = () => {
-    if (player.length === 0) { 
+    if (player.length === 0) {
       setIsHelperTextVisible(true);
     } else {
-      dispatch(actions.startGame(player))
+      dispatch(actions.startGame(player));
     }
-  }
+  };
 
   const onContinueGameHandler = () => {
     if (store.isGameStarted || player !== store.player) return;
-  }
+  };
+
+  const savePlayerName = (player) => {
+    localStorage.setItem('player', player);
+    setPlayer(player);
+  };
 
   return (
     <div className={classes.menu}>
-      <NameInput player={player} onInputValueChangeHandler={(player) => {console.log(player); setPlayer(player)}} isHelperTextVisible={isHelperTextVisible}/>
+      <NameInput
+        player={player}
+        onInputValueChangeHandler={(player) => savePlayerName(player)}
+        isHelperTextVisible={isHelperTextVisible}
+      />
       <span className="separator">♥ ☀ ♦</span>
       <MenuButton onClick={onNewGameHandler} disabled={isHelperTextVisible} path="/game" title="New Game" />
-      <MenuButton onClick={onContinueGameHandler.bind(this)} path="/game" disabled={!store.isGameStarted || isHelperTextVisible || player !== store.player} title="Continue" />
+      <MenuButton
+        onClick={onContinueGameHandler.bind(this)}
+        path="/game"
+        disabled={!store.isGameStarted || isHelperTextVisible || player !== store.player}
+        title="Continue"
+      />
       <MenuButton path="/rating" title="Rating" />
       <MenuButton path="/settings" title="Settings" />
     </div>
