b13e90e5dbf185829b1e37ef7a5ddd38f89e3dd9
Kristina Andreeva <_>
2021-02-20 12:43:16 +0300

    add Menu footer text

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from '../../store/actions';
 import classes from './Menu.css';
 import MenuButton from '../../components/MenuButton/MenuButton';
 import NameInput from '../../components/NameInput/NameInput';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import Logo from '@assets/icons/rss_logo.svg';
 
 const Menu = () => {
   const dispatch = useDispatch();
@@ -25,29 +27,41 @@ const Menu = () => {
   };
 
   const savePlayerName = (player) => {
-    if (player.length > 0 ) localStorage.setItem('player', player);
+    if (player.length > 0) localStorage.setItem('player', player);
     else localStorage.removeItem('player');
     setPlayer(player);
   };
 
   return (
-    <div className={classes.menu}>
-      <NameInput
-        player={player}
-        onInputValueChangeHandler={(player) => savePlayerName(player)}
-        isHelperTextVisible={isHelperTextVisible}
-      />
-      <span className="separator">♥ ☀ ♦</span>
-      <MenuButton onClick={onNewGameHandler} disabled={isHelperTextVisible} path="/game" title="New Game" />
-      <MenuButton
-        onClick={onContinueGameHandler.bind(this)}
-        path="/game"
-        disabled={!state.isGameStarted || isHelperTextVisible || player !== state.player}
-        title="Continue"
-      />
-      <MenuButton path="/rating" title="Rating" />
-      <MenuButton path="/settings" title="Settings" />
-    </div>
+    <Fragment>
+      <div className={classes.menu}>
+        <NameInput
+          player={player}
+          onInputValueChangeHandler={(player) => savePlayerName(player)}
+          isHelperTextVisible={isHelperTextVisible}
+        />
+        <span className="separator">♥ ☀ ♦</span>
+        <MenuButton onClick={onNewGameHandler} disabled={isHelperTextVisible} path="/game" title="New Game" />
+        <MenuButton
+          onClick={onContinueGameHandler.bind(this)}
+          path="/game"
+          disabled={!state.isGameStarted || isHelperTextVisible || player !== state.player}
+          title="Continue"
+        />
+        <MenuButton path="/rating" title="Rating" />
+        <MenuButton path="/settings" title="Settings" />
+      </div>
+      <p className={classes.footer}>
+        Created by{' '}
+        <a rel="noopener noreferrer nofollow" target="_blank" href="https://github.com/kristinand">
+          @kristinand
+        </a>
+        , 2021 /{' '}
+        <a rel="noopener noreferrer nofollow" target="_blank" href="https://rs.school/js/">
+          <SvgIcon component={Logo} style={{ width: '1.7em', height: 'auto' }} viewBox="0 0 552.8 205.3" />
+        </a>
+      </p>
+    </Fragment>
   );
 };
 
