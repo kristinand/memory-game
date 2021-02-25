be3f091711629bf7a0086388df2985cb9158a7bd
Kristina Andreeva <_>
2021-02-25 18:34:04 +0300

    refactor shuffleList with Fisher Yates Algorithm

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,7 +2,22 @@ export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 export function shuffleList(list) {
-  return list.sort(() => Math.random() - 0.5);
+  var currentIndex = list.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = list[currentIndex];
+    list[currentIndex] = list[randomIndex];
+    list[randomIndex] = temporaryValue;
+  }
+
+  return list;
 }
 
 export function listToArray(list, m) {
