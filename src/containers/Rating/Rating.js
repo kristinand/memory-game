19bc9aafb03d13a2e20a0f2d32f1bc3bcf1dd229
Kristina Andreeva <_>
2021-03-18 09:34:13 +0300

    refactor Rating

--- src/containers/Rating/Rating.js
+++ src/containers/Rating/Rating.js
@@ -4,60 +4,47 @@ import { useSelector } from 'react-redux';
 import { formatTime } from '../../utils/functions';
 import Header from '../../components/Header/Header';
 import axios from 'axios';
+import classNames from 'classnames/bind';
 
-let ratingDemoData = [
-  { _id: "1", player: 'Hacker', score: 110, date: new Date() },
-  { _id: "2", player: 'Bob', score: 261, date: new Date('2020', '02', '20', '11', '12') },
-  { _id: "3", player: 'Tom', score: 297, date: new Date('2021', '01', '02', '13', '34') },
-  { _id: "4", player: 'Jess', score: 212, date: new Date('2019', '05', '12', '12', '01') },
-  { _id: "5", player: 'Lily', score: 184, date: new Date('2020', '11', '31', '23', '42') },
-];
+let cx = classNames.bind(classes);
 
 const Rating = () => {
-  const { player: playerName, score, isGameEnded } = useSelector((state) => state);
+  const { player: playerName } = useSelector((state) => state);
   const [ratingData, setRatingData] = useState([]);
   let playersData = 'Loading...';
 
-  const loadDataFromServer = async () => {
+  const loadData = async () => {
     try {
       const res = await axios.get('http://localhost:5000/rating');
       return res.data;
     } catch (err) {
-      if (score > 0 && isGameEnded) {
-        const data = { _d: "0", player: playerName, score, date: new Date() };
-        const playerIndex = ratingDemoData.findIndex((player) => player.player === playerName);
-        if (playerIndex >= 0) ratingDemoData[playerIndex] = data;
-        else ratingDemoData.push(data);
-      }
-      return ratingDemoData;
+      console.log(err);
     }
   };
 
   useEffect(async () => {
-    let data = await loadDataFromServer();
-    setRatingData(data.sort((prev, cur) => prev.score - cur.score));
+    let ratings = await loadData();
+    ratings = ratings.sort((prev, cur) => prev.score - cur.score);
+    setRatingData(ratings);
   }, []);
 
   if (ratingData.length > 0) {
-    playersData= ratingData.map((player, i) => {
-      const playerClass = playerName === player.player ? [classes.player, classes.current].join(' ') : classes.player;
-      player.date = new Date(player.date);
-      const formattedDate = `${player.date.getDate().toString().padStart(2, 0)}.${(player.date.getMonth() + 1)
-        .toString()
-        .padStart(2, 0)}.${player.date.getFullYear()}
-      ${player.date.getHours().toString().padStart(2, 0)}:${player.date.getMinutes().toString().padStart(2, 0)}`;
+    playersData = ratingData.map((player, i) => {
+      const playerClasses = cx({
+        player: true,
+        current: playerName === player.player,
+      });
+
       return (
-        <div key={player._id} className={playerClass}>
+        <div key={player._id} className={playerClasses}>
           <span>{i + 1}</span>
           <span>{player.player}</span>
           <span>{formatTime(player.score)}</span>
-          <span>{formattedDate}</span>
+          <span>{new Date(player.date).toLocaleString()}</span>
         </div>
       );
     });
   }
-
-
 
   return (
     <Fragment>
