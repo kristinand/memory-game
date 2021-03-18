804554c932e32c1456d40bcd3da0d5e4bd855dd7
Kristina Andreeva <_>
2021-03-18 09:09:49 +0300

    refactor shuffleList

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,16 +2,14 @@ export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 export function shuffleList(list) {
-  var currentIndex = list.length, temporaryValue, randomIndex;
+  let currentIndex = list.length,
+    temporaryValue,
+    randomIndex;
 
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = list[currentIndex];
     list[currentIndex] = list[randomIndex];
     list[randomIndex] = temporaryValue;
@@ -72,4 +70,4 @@ export function fillCards(cards, level, coverColor) {
     });
   }
   return cards;
-};
+}
