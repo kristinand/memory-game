afe921bdd7d9c53d59f414e40430f8ac85a0a530
Kristina Andreeva <_>
2021-04-01 23:23:32 +0300

    refactor: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import classes from './Header.css';
+import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import Back from '@assets/icons/left.svg';
+import classes from './Header.css';
 import IconButton from '../IconButton/IconButton';
 
-const Header = (props) => {
+const Header = ({ title }) => {
   const history = useHistory();
   return (
     <div className={classes.Header}>
       <span className={classes.button}>
         <IconButton component={Back} text="Back to Menu" onClick={() => history.push('/')} />
       </span>
-      <span className={classes.title}>{props.title}</span>
+      <span className={classes.title}>{title}</span>
     </div>
   );
 };
 
 export default Header;
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
