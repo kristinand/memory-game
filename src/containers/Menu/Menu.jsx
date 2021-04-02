0c19f79a444094672903fa1570b61acd9baaa1bd
Kristina Andreeva <_>
2021-04-02 22:58:30 +0300

    refactor: Menu, add eslint-import-resolver-webpack

--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -1,41 +1,36 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import * as actions from '../../store/actions';
-import classes from './Menu.css';
-import MenuButton from '../../components/MenuButton/MenuButton';
-import IconButton from '../../components/IconButton/IconButton';
+import classNames from 'classnames/bind';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Logo from '@assets/icons/rss_logo.svg';
 import Login from '@assets/icons/right.svg';
 import Logout from '@assets/icons/left.svg';
-import classNames from 'classnames/bind';
+import * as actions from '../../store/actions';
+import MenuButton from '../../components/MenuButton/MenuButton';
+import IconButton from '../../components/IconButton/IconButton';
+import classes from './Menu.css';
 
-let cx = classNames.bind(classes);
+const cx = classNames.bind(classes);
 
 const Menu = () => {
   const dispatch = useDispatch();
-  const state = useSelector((state) => state);
+  const state = useSelector((store) => store);
   const [helperText, setHelperText] = useState('');
   const [player, setPlayer] = useState(localStorage.getItem('player') || state.player);
 
-  const onContinueGameHandler = () => {
-    if (localStorage.getItem('gameData') === null || localStorage.getItem('player') !== player) return;
-  };
-
-  const onInputValueChangeHandler = (player) => {
+  const onInputValueChangeHandler = (value) => {
     if (state.isLoggedIn) return;
-    player = player.trim();
-    setPlayer(player);
+    setPlayer(value.trim());
   };
 
   const login = () => {
     const playerRegEx = new RegExp(/^[a-zA-Z]{3,10}$/);
     if (player.length === 0) {
-      setHelperText('Please, enter your name')
+      setHelperText('Please, enter your name');
     } else if (player.length < 3) {
-      setHelperText('Your name should contain at least 3 characters')
+      setHelperText('Your name should contain at least 3 characters');
     } else if (player.match(playerRegEx) === null) {
-      setHelperText('Only latin characters allowed')
+      setHelperText('Only latin characters allowed');
     } else {
       localStorage.setItem('player', player);
       dispatch(actions.login(player));
@@ -52,11 +47,11 @@ const Menu = () => {
 
   const helperTextClasses = cx({
     input: true,
-    inputDanger: helperText.length > 0
-  })
+    inputDanger: helperText.length > 0,
+  });
 
   return (
-    <Fragment>
+    <>
       <div className={classes.menu}>
         <div className={classes.NameInput}>
           <div className={classes.inputContainer}>
@@ -75,11 +70,7 @@ const Menu = () => {
               title={state.isLoggedIn ? 'Logout' : 'Login'}
             />
           </div>
-          {helperText.length > 0 ? (
-            <p className={classes.helperText}>{helperText}</p>
-          ) : (
-            ''
-          )}
+          {helperText.length > 0 ? <p className={classes.helperText}>{helperText}</p> : ''}
         </div>
 
         <span className="separator">♥ ☀ ♦</span>
@@ -91,28 +82,35 @@ const Menu = () => {
           title="New Game"
         />
         <MenuButton
-          onClick={onContinueGameHandler}
-          path={localStorage.getItem('gameData') !== null && localStorage.getItem('player') === JSON.parse(localStorage.getItem('gameData')).player ? '/game' : '/'}
+          path={
+            localStorage.getItem('gameData') !== null
+            && localStorage.getItem('player') === JSON.parse(localStorage.getItem('gameData')).player
+              ? '/game'
+              : '/'
+          }
           disabled={
-            !state.isLoggedIn || state.isGameEnded || localStorage.getItem('gameData') === null || localStorage.getItem('player') !== JSON.parse(localStorage.getItem('gameData')).player
+            !state.isLoggedIn || state.isGameEnded || localStorage.getItem('gameData') === null
+            || localStorage.getItem('player') !== JSON.parse(localStorage.getItem('gameData')).player
           }
           title="Continue"
         />
         <MenuButton path="/rating" title="Rating" />
-        <MenuButton disabled={!state.isLoggedIn} path={!state.isLoggedIn ? '': '/settings'} title="Settings" />
+        <MenuButton disabled={!state.isLoggedIn} path={!state.isLoggedIn ? '' : '/settings'} title="Settings" />
         <MenuButton path="/about" title="About" />
       </div>
       <p className={classes.footer}>
-        Created by{' '}
+        Created by
+        {' '}
         <a rel="noopener noreferrer nofollow" target="_blank" href="https://github.com/kristinand">
           @kristinand
         </a>
-        , 2021 /{' '}
+        , 2021 /
+        {' '}
         <a rel="noopener noreferrer nofollow" target="_blank" href="https://rs.school/js/">
           <SvgIcon component={Logo} style={{ width: '1.7em', height: 'auto' }} viewBox="0 0 552.8 205.3" />
         </a>
       </p>
-    </Fragment>
+    </>
   );
 };
 
