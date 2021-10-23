f7120850ef08ed2e66c5c45a4bd002ee01ecd7c3
Kristina Andreeva <tabbyk@ya.ru>
2021-10-23 22:00:13 +0300

    fix saveRating

--- server/controllers/scoreController.js
+++ server/controllers/scoreController.js
@@ -1,6 +1,7 @@
 const Score = require('../models/Score.js');
 const catchAsync = require('../utils/catchAsync');
 const APIFeatures = require('../utils/APIFeatures');
+const { listenerCount } = require('../app.js');
 
 exports.getAllRatings = catchAsync(async (req, res) => {
   // one more way to sort
@@ -22,17 +23,14 @@ exports.getAllRatings = catchAsync(async (req, res) => {
 });
 
 exports.saveRating = catchAsync(async (req, res) => {
-  const { player, score } = req.body;
+  const doc = await Score.findOne({ player: req.body.player });
 
-  await Score.findOneAndUpdate(
-    { player },
-    {
-      $set: {
-        score,
-      },
-    },
-    { upsert: true, new: true, runValidators: true },
-  );
+  if (doc) {
+    doc.score = req.body.score;
+    await doc;
+  } else {
+    await Score.create(req.body);
+  }
 
   res.status(200).json({
     status: 'success',
