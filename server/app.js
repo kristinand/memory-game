c547e78c97d4fa67632187d8d3599fe4bdf7e850
Kristina Andreeva <tabbyk@ya.ru>
2021-10-25 22:48:20 +0300

    fixes

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', '');
+  res.setHeader('Access-Control-Allow-Methods', '*');
   next();
 });
 
