c9cc8e57bc080b663ca37215e792f62dd89966ef
Kristina Andreeva <tabbyk@ya.ru>
2022-10-20 19:46:52 +0300

    fix types

--- src/utils/functions/listToArray.ts
+++ src/utils/functions/listToArray.ts
@@ -1,16 +1,17 @@
 // [] 1xK -> [[],[],[]] PxM
-export function listToArray(list: unknown[], m: number): unknown[] {
+export function listToArray<T = any>(list: T[], m: number): T[][] {
   let k = list.length / m;
   let p = m;
 
   if (k < m) {
     k = m;
     p = list.length / m;
-  } 
+  }
 
   if (Number.isInteger(k)) {
     const array = new Array(p).fill([]);
     return array.map((_, i) => list.slice(Number(i) * k, Number(i) * k + k));
   }
-  return list;
+
+  return [list];
 }
