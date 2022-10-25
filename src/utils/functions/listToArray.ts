bcd89e099abdd632484683e06b9429c628479f05
Kristina Andreeva <tabbyk@ya.ru>
2022-10-25 14:56:06 +0300

    refactor utils

--- src/utils/functions/listToArray.ts
+++ src/utils/functions/listToArray.ts
@@ -1,9 +1,16 @@
 export function listToArray<T>(list: T[], column: number): T[][] {
-  const row = list.length / column;
+  let col = column;
+  let row = list.length / col;
+  const storedRow = row;
+
+  if (col > row) {
+    row = col;
+    col = storedRow;
+  }
 
   if (Number.isInteger(row)) {
     const array = new Array(row).fill([]);
-    return array.map((_, i) => list.slice(i * column, i * column + column));
+    return array.map((_, i) => list.slice(i * col, i * col + col));
   }
 
   return [list];
